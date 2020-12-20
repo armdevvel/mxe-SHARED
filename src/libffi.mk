9b94eec052f1ab8310e800f7f54bed806f31a784
pahaze <pahaze@pop-os.localdomain>
2020-12-20 05:20:39 -0500

    Fix libffi source.

--- src/libffi.mk
+++ src/libffi.mk
@@ -8,7 +8,7 @@ $(PKG)_CHECKSUM := d06ebb8e1d9a22d19e38d63fdb83954253f39bedc5d46232a05645685722c
 $(PKG)_GH_CONF  := atgreen/libffi/tags, v
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://www.mirrorservice.org/sites/sourceware.org/pub/$(PKG)/$($(PKG)_FILE)
+$(PKG)_URL      := https://github.com/armdevvel/libffi/releases/download/v3.2.1/libffi-3.2.1.tar.xz
 $(PKG)_URL_2    := https://sourceware.org/pub/$(PKG)/$($(PKG)_FILE)
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
 $(PKG)_DEPS     := cc
@@ -22,10 +22,6 @@ define $(PKG)_BUILD
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 
-    '$(TARGET)-gcc' \
-        -W -Wall -Werror -std=c99 -pedantic \
-        '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-libffi.exe' \
-        `'$(TARGET)-pkg-config' libffi --cflags --libs`
 endef
 
 define $(PKG)_BUILD_$(BUILD)
