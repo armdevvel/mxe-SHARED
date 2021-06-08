f14193532728ffb1bc6382df63c9cb70b23c0641
pahaze <pahaze200@protonmail.com>
2021-06-08 19:12:27 -0400

    Remove Ubuntu-distro specific code.

--- mxe-curl-inst.sh
+++ mxe-curl-inst.sh
@@ -1,38 +1,5 @@
 echo "Clone MXE from my source that way ARM is included :)"
 git clone https://github.com/armdevvel/mxe --depth=1
-echo "Install all necessary packages! -- Ubuntu distros"
-sudo apt-get install \
-    autoconf \
-    automake \
-    autopoint \
-    bash \
-    bison \
-    bzip2 \
-    flex \
-    g++ \
-    g++-multilib \
-    gettext \
-    git \
-    gperf \
-    intltool \
-    libc6-dev-i386 \
-    libgdk-pixbuf2.0-dev \
-    libltdl-dev \
-    libssl-dev \
-    libtool-bin \
-    libxml-parser-perl \
-    lzip \
-    make \
-    openssl \
-    p7zip-full \
-    patch \
-    perl \
-    python \
-    ruby \
-    sed \
-    unzip \
-    wget \
-    xz-utils
 echo "cd mxe!"
 cd mxe
 echo "Download LLVM-MinGW!"
