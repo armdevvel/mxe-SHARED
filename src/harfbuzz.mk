f677d4efce13be2ee63a84a82b0063efe52dc0bb
pahaze <pahaze200@protonmail.com>
2021-12-12 14:33:08 -0500

    Update harfbuzz.mk

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -1,12 +1,11 @@
-# This file is part of MXE. See LICENSE.md for licensing information.
-
 PKG             := harfbuzz
 $(PKG)_WEBSITE  := https://wiki.freedesktop.org/www/Software/HarfBuzz/
-$(PKG)_DESCR    := HarfBuzz
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 2.7.2
-$(PKG)_CHECKSUM := 8ec112ee108642477478b75fc7906422abed404d7530e47ba0a4875f553f1b59
-$(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases
+$(PKG)_CHECKSUM := e7e4041996a354f7772ee865e2658b4d008947f5d8464dbac310592a28666a98
+$(PKG)_SUBDIR   := harfbuzz-$($(PKG)_VERSION)
+$(PKG)_FILE     := harfbuzz-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://github.com/armdevvel/harfbuzz/releases/download/v2.7.2/harfbuzz-2.7.2.tar.gz
 $(PKG)_DEPS     := cc cairo freetype-bootstrap glib icu4c
 
 define $(PKG)_BUILD
