74d66256921b94e84b21c6f7ca08b04b333eb997
pahaze <pahaze200@protonmail.com>
2022-04-08 12:07:06 -0400

    Update install script to LLVM 14

--- mxe-ubuntu-install.sh
+++ mxe-ubuntu-install.sh
@@ -46,11 +46,11 @@ git clone https://github.com/armdevvel/mxe --depth=1 armmxe
 echo "cd mxe!"
 cd armmxe
 echo "Download LLVM-MinGW!"
-mkdir usr && cd usr && wget https://github.com/armdevvel/llvm-mingw/releases/download/13.0/armv7-only-llvm-mingw-linux-x86_64.tar.xz
+mkdir usr && cd usr && wget https://github.com/armdevvel/llvm-mingw/releases/download/14.0/armv7-only-llvm-mingw-linux-x86_64.tar.xz
 echo "Extract LLVM-MinGW!"
 tar -xf armv7-only-llvm-mingw-linux-x86_64.tar.xz
 echo "Setup known packages that work! This may take a while... Hold on tight!~"
-cd .. && make libpng cmake sdl2 sdl tiff jpeg ccache lame libxml++ libxml2 libxslt libyaml libzip libwebp libusb1 sdl_image sdl_mixer sdl2_mixer zlib yasm dbus pcre boost icu4c
+cd .. && make libpng cmake sdl2 sdl tiff jpeg ccache lame libxml++ libxml2 libxslt libyaml libzip libwebp libusb1 sdl_image sdl_mixer sdl2_image sdl2_mixer zlib yasm dbus pcre icu4c
 echo "Adding MXE to your PATH (bash)"
 echo "\n" >> ~/.bashrc
 echo "export PATH=/home/$USER/armmxe/usr/bin"':$PATH' >> ~/.bashrc
