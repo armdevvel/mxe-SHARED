69e31a89964f3f86b507162776971dc58c8f341d
pahaze <pahaze200@protonmail.com>
2022-04-08 11:50:26 -0400

    Update OpenSSL

--- src/openssl.mk
+++ src/openssl.mk
@@ -3,46 +3,20 @@
 PKG             := openssl
 $(PKG)_WEBSITE  := https://www.openssl.org/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.1.1h
-$(PKG)_CHECKSUM := 5c9ca8774bd7b03e5784f26ae9e9e6d749c9da2438545077e6b3d755a06595d9
+$(PKG)_VERSION  := 1.1.1k
+$(PKG)_CHECKSUM := ded4c2b3efaa1787ddc9c7b6bc121d123c904f1b2a3a381078ee5e75f0382fb4
 $(PKG)_SUBDIR   := openssl-$($(PKG)_VERSION)
-$(PKG)_FILE     := openssl-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://www.openssl.org/source/$($(PKG)_FILE)
-$(PKG)_URL_2    := https://www.openssl.org/source/old/$(call tr,$([a-z]),,$($(PKG)_VERSION))/$($(PKG)_FILE)
+$(PKG)_FILE     := openssl-src.tar.gz
+$(PKG)_URL      := https://download.pahaze.net/ARM/mxe/OpenSSL/src/MSVC.tar.gz
 $(PKG)_DEPS     := cc zlib
 
-define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://www.openssl.org/source/' | \
-    $(SED) -n 's,.*openssl-\([0-9][0-9a-z.]*\)\.tar.*,\1,p' | \
-    $(SORT) -V | \
-    tail -1
-endef
-
 define $(PKG)_BUILD
-    # remove previous install
-    rm -rfv '$(PREFIX)/$(TARGET)/include/openssl'
-    rm -rfv '$(PREFIX)/$(TARGET)/bin/engines'
-    rm -fv '$(PREFIX)/$(TARGET)/'*/{libcrypto*,libssl*}
-    rm -fv '$(PREFIX)/$(TARGET)/lib/pkgconfig/'{libcrypto*,libssl*,openssl*}
+    cp $(PREFIX)/../resources/MSVC-OpenSSL/lib/* $(PREFIX)/$(TARGET)/lib
+	cp -r $(PREFIX)/../resources/MSVC-OpenSSL/include/* $(PREFIX)/$(TARGET)/include
+	cp $(PREFIX)/../resources/MSVC-OpenSSL/bin/* $(PREFIX)/$(TARGET)/bin
 
-    cd '$(1)' && CC='$(TARGET)-gcc' RC='$(TARGET)-windres' ./Configure \
-        @openssl-target@ \
-        zlib \
-        $(if $(BUILD_STATIC),no-,)shared \
-        no-capieng \
-        --prefix='$(PREFIX)/$(TARGET)'
-    
-    # sadly, -j 1 is intentional: the generated makefile contains implicit dependencies ;(
-    $(MAKE) -C '$(1)' all install_sw -j 1 \
-        CC='$(TARGET)-gcc' \
-        RANLIB='$(TARGET)-ranlib' \
-        AR='$(TARGET)-ar' \
-        RC='$(TARGET)-windres' \
-        CROSS_COMPILE='$(TARGET)-' \
-        $(if $(BUILD_SHARED), ENGINESDIR='$(PREFIX)/$(TARGET)/bin/engines')
+	sed 's,%PREFIX%,$(PREFIX)/$(TARGET),' \
+		< '$(SOURCE_DIR)/libcrypto.pc' > '$(PREFIX)/$(TARGET)/lib/pkgconfig/libcrypto.pc'
+	sed 's,%PREFIX%,$(PREFIX)/$(TARGET),' \
+		< '$(SOURCE_DIR)/libssl.pc' > '$(PREFIX)/$(TARGET)/lib/pkgconfig/libssl.pc'
 endef
-
-$(PKG)_BUILD_i686-w64-mingw32   = $(subst @openssl-target@,mingw,$($(PKG)_BUILD))
-$(PKG)_BUILD_x86_64-w64-mingw32 = $(subst @openssl-target@,mingw64,$($(PKG)_BUILD))
-$(PKG)_BUILD_armv7-w64-mingw32   = $(subst @openssl-target@,armv7-w64-mingw32,$($(PKG)_BUILD))
-$(PKG)_BUILD_aarch64-w64-mingw32 = $(subst @openssl-target@,aarch64-w64-mingw32,$($(PKG)_BUILD))
