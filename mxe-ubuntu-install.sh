0fefebdc72c496de51528660f37c606e36e1ec44
pahaze <69173292+pahaze@users.noreply.github.com>
2022-07-11 00:35:43 -0400

    Update mxe-ubuntu-install.sh
    bleh

--- mxe-ubuntu-install.sh
+++ mxe-ubuntu-install.sh
@@ -42,16 +42,17 @@ sudo apt update && sudo apt install \
 echo "Install meson (as sudo)"
 sudo pip3 install meson
 echo "Clone MXE from my source that way ARM is included :)"
-git clone https://github.com/armdevvel/mxe --depth=1 armmxe
+git clone https://github.com/armdevvel/mxe-UNSTABLE --depth=1 armmxe-unstable
 echo "cd mxe!"
-cd armmxe
+cd armmxe-unstable
 echo "Download LLVM-MinGW!"
 mkdir usr && cd usr && wget https://github.com/armdevvel/llvm-mingw/releases/download/14.0/armv7-only-llvm-mingw-linux-x86_64.tar.xz
 echo "Extract LLVM-MinGW!"
 tar -xf armv7-only-llvm-mingw-linux-x86_64.tar.xz
 echo "Setup known packages that work! This may take a while... Hold on tight!~"
+echo "This may FAIL. These have not been tested yet!"
 cd .. && make libpng cmake sdl2 sdl tiff jpeg ccache lame libxml++ libxml2 libxslt libyaml libzip libwebp libusb1 sdl_image sdl_mixer sdl2_image sdl2_mixer zlib yasm dbus pcre icu4c
 echo "Adding MXE to your PATH (bash)"
 echo "\n" >> ~/.bashrc
-echo "export PATH=/home/$USER/armmxe/usr/bin"':$PATH' >> ~/.bashrc
+echo "export PATH=/home/$USER/armmxe-unstable/usr/bin"':$PATH' >> ~/.bashrc
 echo "Finished!"
