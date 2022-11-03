0613550b5c084a7af3428ce0ebc73d7df1de3bac
Treeswift <treeswift@mailbox.org>
2022-11-03 08:26:01 -0700

    switch to mbedtls-SRTP fork -- revert to restore

--- src/mbedtls.mk
+++ src/mbedtls.mk
@@ -4,15 +4,15 @@ PKG             := mbedtls
 $(PKG)_WEBSITE  := https://www.trustedfirmware.org/projects/mbed-tls/
 $(PKG)_DESCR    := Embeddable TLS implementation (Apache 2.0 license)
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.2.1
-$(PKG)_CHECKSUM := d0e77a020f69ad558efc660d3106481b75bd3056d6301c31564e04a0faae88cc
-$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_VERSION  := 2.2.0
+$(PKG)_CHECKSUM := d77cd2edea4ce46e7ab0c2500298212b6475af6e51210ef9c5a6834283c6727f
+$(PKG)_SUBDIR   := $(PKG)-SRTP-$(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/Mbed-TLS/$(PKG)/archive/refs/tags/v$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://github.com/laandersson/$(PKG)-SRTP/archive/refs/tags/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc
 
 define $(PKG)_BUILD
-    cd '$(BUILD_DIR)' && $(TARGET)-cmake -v \
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake \
 		-DENABLE_TESTING=Off \
 		-DUSE_SHARED_MBEDTLS_LIBRARY=$(if $(BUILD_SHARED),On,Off) \
 		'$(SOURCE_DIR)'
