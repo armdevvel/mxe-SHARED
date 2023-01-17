1f08b4b713ffb8cc65925f11fccbb6d61436339d
pahaze <pahaze200@protonmail.com>
2023-01-16 22:39:48 -0500

    Update src/cc.mk, add src/llvm-mingw.mk

---
+++ src/llvm-mingw.mk
@@ -0,0 +1,14 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := llvm-mingw
+$(PKG)_WEBSITE  := http://www.github.com/mstorsjo/llvm-mingw
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 14
+$(PKG)_CHECKSUM := d294d5435555150414fe6bc403d8b807a00611fbed5622d236719be2410128b8
+$(PKG)_SUBDIR   :=
+$(PKG)_FILE     := armv7-only-llvm-mingw-linux-x86_64.tar.xz
+$(PKG)_URL      := https://github.com/armdevvel/llvm-mingw/releases/download/14.0/armv7-only-llvm-mingw-linux-x86_64.tar.xz
+
+define $(PKG)_BUILD
+	cp -r '$(1)'* '$(PREFIX)'
+endef
