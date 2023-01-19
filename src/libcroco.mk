507e3f1a916a46f3f9f7ae55819ea8d75b48ab11
TreeSwift <treeswift@mailbox.org>
2023-01-19 04:28:10 -0800

    libcroco: --disable-Bsymbolic

--- src/libcroco.mk
+++ src/libcroco.mk
@@ -21,6 +21,7 @@ define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
         $(MXE_CONFIGURE_OPTS) \
         --disable-gtk-doc \
+        --disable-Bsymbolic \
         $(PKG_CONFIGURE_OPTS)
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_PROGRAMS)
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_PROGRAMS)
