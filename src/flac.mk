8b362f7a52438c14468154e68856b63721700299
TreeSwift <treeswift@mailbox.org>
2023-01-19 03:52:35 -0800

    flac: __chkstk fix

--- src/flac.mk
+++ src/flac.mk
@@ -32,6 +32,6 @@ define $(PKG)_BUILD
         --enable-cpplibs \
         --enable-ogg \
         --disable-oggtest
-    $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+    $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS= LDFLAGS='`$(MXE_INTRINSIC_SH) chkstk.S.obj`'
     $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 endef
