4bca2a0a87b84ebadb63d084fef7cd3ae5c758e8
pahaze <pahaze200@protonmail.com>
2024-10-12 20:00:58 -0400

    Revert to LLVM 14

--- src/llvm-mingw.mk
+++ src/llvm-mingw.mk
@@ -2,8 +2,8 @@
 
 PKG             := llvm-mingw
 $(PKG)_WEBSITE  := http://www.github.com/mstorsjo/llvm-mingw
-$(PKG)_VERSION  := 18.0
-$(PKG)_CHECKSUM := 393abf26e25bcf03a923310389f874fc0a3134619a54eedb5e84c5ff43cbd664
+$(PKG)_VERSION  := 14.0-opengl
+$(PKG)_CHECKSUM := a9d040c08dded13d3d3d5f958cc1bda2060c415b40ca4d719a78efaa6e82f272
 $(PKG)_FILE     := armv7-only-llvm-mingw-linux-x86_64.tar.xz
 $(PKG)_URL      := https://github.com/armdevvel/llvm-mingw/releases/download/$($(PKG)_VERSION)/armv7-only-llvm-mingw-linux-x86_64.tar.xz
 $(PKG)_DEPS     := $(BUILD)~snakeoil
