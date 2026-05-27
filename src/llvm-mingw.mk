ef8643d9352314a81f770b1232c0b00acfc75f43
Frye Lumier <lumier@fryelum.dev>
2026-05-27 01:57:42 -0400

    llvm-mingw.mk: Update to 22.1.6

--- src/llvm-mingw.mk
+++ src/llvm-mingw.mk
@@ -2,8 +2,8 @@
 
 PKG                  := llvm-mingw
 $(PKG)_WEBSITE       := http://www.github.com/mstorsjo/llvm-mingw
-$(PKG)_VERSION       := 14.0-debug-hotfix1
-$(PKG)_CHECKSUM      := 195620dd64411ac86d1273b706c8b85fb81ad57d4028c2b00e9cf4e192f0603a
+$(PKG)_VERSION       := 22.1.6
+$(PKG)_CHECKSUM      := a288948b27e1c98c59924d7f1eb89eee737722cbf448e80cc36f34836b6f392b
 $(PKG)_FILE          := armv7-only-llvm-mingw-linux-x86_64.tar.xz
 $(PKG)_URL           := https://github.com/armdevvel/llvm-mingw/releases/download/$($(PKG)_VERSION)/armv7-only-llvm-mingw-linux-x86_64.tar.xz
 $(PKG)_TARGETS       := $(BUILD)
